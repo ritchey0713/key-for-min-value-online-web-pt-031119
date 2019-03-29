5868ce78fce79b6aba8af08919ff29e4c99fefe6
David Ritchey <ritchey0713@gmail.com>
2019-03-29 23:04:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,8 +8,8 @@
 # compare old values to new values 
 
 def key_for_min_value(name_hash)
-  low_key = nil  
-  low_val = nil
+  low_key  
+  low_val 
   name_hash.each do |name, numbers|
     if low_val == nil || numbers < low_val
       low_val = numbers 
